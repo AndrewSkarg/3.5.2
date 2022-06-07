57d925487937548b0ad75c3634707c624b54bc99
AndrewSkarg <99492436+AndrewSkarg@users.noreply.github.com>
2022-06-07 16:32:59 +0300

    Update Main.java

--- src/com/exam/Main.java
+++ src/com/exam/Main.java
@@ -1,10 +1,9 @@
 package com.exam;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
+
 
 public class Main {
 
